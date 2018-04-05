2ed050dac4063aed2fbad682c217477a018bd2c5
Patrick East <patrick.east@icloud.com>
2018-04-04 21:06:38 -0600

    First commit. Added files, nmp installed, added keys, and started code.

---
+++ liri.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const argument = process.argv[2];
+const spotify = new Spotify(keys.spotify);
+const client = new Twitter(keys.twitter);
+const tweet = process.argv[3];
+const spotify = process.argv[3];
+const movie = process.argv[3];
+const doIt = process.argv[3];
+
+if (math === "my-tweets") {
+    console.log(tweet)
+} else if (math === "spotify-this-song") {
+    console.log(spotify)
+} else if (math === "spotify-this-song") {
+    console.log(spotify)
+} else if (math === "movie-this") {
+    console.log(movie)
+} else if (math === "do-what-it-says") {
+    console.log(doThis)
+} 
