58e8819fbea798ad545952b13efe5d1d8e5028ca
Patrick East <patrick.east@icloud.com>
2018-04-08 20:30:52 -0600

    Started Spotify.

--- liri.js
+++ liri.js
@@ -1,51 +1,59 @@
 require("dotenv").config();
 const keys = require("./keys.js");
-const Twitter = require("twitter");
-// const spotify = require("spotify");
+
 const request = require("request");
-
-// const spotify = new Spotify(keys.spotify);
+const Twitter = require("twitter");
+const Spotify = require('node-spotify-api');
+const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
-const userInput = process.argv.slice(2).join("");
+const userInput = process.argv.splice(2).join("");
 
-if (userInput === "my-tweets") {myTweets()
-} else if (userInput === "spotify-this-song") {spotifyThis()
-} else if (userInput === "movie-this") {movieThis()
-} else if (userInput === "do-what-it-says") {doWhatItSays()
+if (userInput === "my-tweets") {
+    myTweets();
+} else if (userInput === "spotify-this-song") {
+    spotifyThis();
+} else if (userInput === "movie-this") {
+    movieThis();
+} else if (userInput === "do-what-it-says") {
+    doWhatItSays();
 };
-
 
 function myTweets() {
-
-const params = { screen_name: "EastPatrick", count: "20" };
-client.get('statuses/user_timeline', params, function (error, tweets, response) {
-    if (!error) {
-
-        let tweetObjects = JSON.parse(response.body);
-        for (let i = 0; i < tweets.length; i++) {
-            console.log(tweets[i].text);
-        };
-
-    } else {
-        console.log(error);
-    }
-});
+    const params = { screen_name: "EastPatrick", count: "20" };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            let tweetObject = JSON.parse(response.body);
+            for (let i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].created_at);
+                console.log(tweets[i].text);
+                console.log("------------------------------");
+            };
+        } else {
+            console.log(error);
+        }
+    });
 };
 
-// const argument = process.argv[2];
-// const tweet = process.argv[3];
-// // const spotify = process.argv[3];
-// // const movie = process.argv[3];
-// // const doIt = process.argv[3];
+function spotifyThis() {
+    // const spotifyInput = process.argv.splice(3).join("");
+    
+    spotify.search({ type: 'track', query: "All the Small Things", limit: "1" }, function (err, data) {
+        const songId = data.tracks.items[0].id;
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log(data.tracks.items[0].artists[0].name);
+        console.log(data.tracks.items[0].name);
+        console.log(data.tracks.items[0].preview_url);
+        console.log(data.tracks.items[0].album.name);
+        console.log(songId);
 
-// if (math === "my-tweets") {
-//     console.log(tweet)
-// } else if (math === "spotify-this-song") {
-//     console.log(spotify)
-// } else if (math === "spotify-this-song") {
-//     console.log(spotify)
-// } else if (math === "movie-this") {
-//     console.log(movie)
-// } else if (math === "do-what-it-says") {
-//     console.log(doThis)
-// } 
+    });
+    // .request('https://api.spotify.com/v1/tracks/' + songId)
+    // .then(function (data) {
+    //     console.log(data);
+    // })
+    // .catch(function (err) {
+    //     console.error('Error occurred: ' + err);
+    // });
+};
