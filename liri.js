2eea96dbe997c882fdf3add2a470cf619b386fec
Patrick East <patrick.east@icloud.com>
2018-04-09 13:03:05 -0600

    Completed Spotify.

--- liri.js
+++ liri.js
@@ -6,17 +6,33 @@ const Twitter = require("twitter");
 const Spotify = require('node-spotify-api');
 const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
-const userInput = process.argv.splice(2).join("");
+const userInput = process.argv[2];
 
-if (userInput === "my-tweets") {
-    myTweets();
-} else if (userInput === "spotify-this-song") {
-    spotifyThis();
-} else if (userInput === "movie-this") {
-    movieThis();
-} else if (userInput === "do-what-it-says") {
-    doWhatItSays();
-};
+switch (userInput) {
+    case "my-tweets":
+        myTweets();
+        break;
+
+    case "spotify-this-song":
+        if (process.argv[3] === undefined) {
+            let songName = "The Sign";
+            spotifyThis(songName);
+        } else {
+            let songName = process.argv.slice(3).join(' ');
+            spotifyThis(songName);
+        }
+        break;
+
+    case "movie-this":
+        movieThis();
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+
+    default:
+}
 
 function myTweets() {
     const params = { screen_name: "EastPatrick", count: "20" };
@@ -34,26 +50,23 @@ function myTweets() {
     });
 };
 
-function spotifyThis() {
-    // const spotifyInput = process.argv.splice(3).join("");
-    
-    spotify.search({ type: 'track', query: "All the Small Things", limit: "1" }, function (err, data) {
-        const songId = data.tracks.items[0].id;
-        if (err) {
+function spotifyThis(songName) {
+    spotify.search({
+        type: 'track',
+        query: songName
+    }, function (err, data) {
+        if (!err) {
+            data.tracks.items.forEach(function (object) {
+                console.log("------------------------");
+                console.log("Artist Name " + object.album.artists[0].name);
+                console.log("Song Name: " + object.name);
+                console.log("Preview on Spotify: " + object.preview_url);
+                console.log("Album Name: " + object.album.name);
+                console.log("------------------------");
+            })
+        } else {
             return console.log('Error occurred: ' + err);
         }
-        console.log(data.tracks.items[0].artists[0].name);
-        console.log(data.tracks.items[0].name);
-        console.log(data.tracks.items[0].preview_url);
-        console.log(data.tracks.items[0].album.name);
-        console.log(songId);
 
-    });
-    // .request('https://api.spotify.com/v1/tracks/' + songId)
-    // .then(function (data) {
-    //     console.log(data);
-    // })
-    // .catch(function (err) {
-    //     console.error('Error occurred: ' + err);
-    // });
-};
+    })
+}
